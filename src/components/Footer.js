48875ef49c733044053a8ac606443d7eb2140076
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2024-01-21 17:48:47 +0300

    updated nick name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by richjr</h3>
+          <h3>Designed and Developed by Rich</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>
